45683857a93dbe4ca341132f591f7595e58afa64
Avinash Kondareddy <akndr41@gmail.com>
2020-03-03 13:28:11 -0800

    Update polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,22 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-# Terminate any currently running instances
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
 killall -q polybar
 
-# Pause while killall completes
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr" > /dev/null; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example -c ~/.config/polybar/config &
-  done
-else
-  polybar --reload example -c ~/.config/polybar/config &
-fi
-
-# Launch bar(s)
-# polybar dummy -q &
-# polybar top -q &
-# polybar bottom -q  &
-
-echo "polybars launched..."
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini main &
